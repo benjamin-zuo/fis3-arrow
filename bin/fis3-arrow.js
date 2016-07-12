9cd02a6c5ffd6e5fe6cafc6f8477e342ef8b54a3
benjamin-zuo <cuew1987@163.com>
2016-07-12 18:03:14 +0800

    v0.0.13 release

--- bin/fis3-arrow.js
+++ bin/fis3-arrow.js
@@ -1,11 +1,5 @@
 #!/usr/bin/env node
 
-var fis = require('../index.js');
-
-fis.cli.run(process.argv.slice(2));
-
-
-/*
 var Liftoff = require('liftoff');
 var argv = require('minimist')(process.argv.slice(2));
 var path = require('path');
@@ -23,19 +17,15 @@ cli.launch({
     cwd: argv.r || argv.root,
     configPath: argv.f || argv.file
 }, function(env) {
-    var fis = require(!env.modulePath ? '../index.js', env.modulePath);
+    var fis = require(!env.modulePath ? '../index.js' : env.modulePath);
     
     process.title = this.name + ' ' + process.argv.slice(2).join(' ') + ' [ ' + env.cwd + ' ]';
     /**
      * 配置插件查找路径，优先查找本地项目里面的 node_modules，然后
-     * 才是全局环境下面安装的 fis3 目录里面的 node_modules
-     */
-    
-    /*
+     * 才是全局环境下面安装的 fisa 目录里面的 node_modules
+    */
     fis.require.paths.unshift(path.join(env.cwd, 'node_modules'));
     fis.require.paths.push(path.join(path.dirname(__dirname), 'node_modules'));
     fis.cli.name = this.name;
     fis.cli.run(argv, env);
 });
-
-*/
