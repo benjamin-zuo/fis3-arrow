026a649ddea44e3f8b2f47ba6f4cea853cccc3be
benjamin-zuo <cuew1987@163.com>
2016-07-15 13:54:26 +0800

    v0.0.19 release

--- bin/fis3-arrow.js
+++ bin/fis3-arrow.js
@@ -4,28 +4,34 @@ var Liftoff = require('liftoff');
 var argv = require('minimist')(process.argv.slice(2));
 var path = require('path');
 var cli = new Liftoff({
-    name: 'fisa',
-    processTitle: 'fisa',
-    moduleName: 'fisa',
-    configName: 'fis-conf',
-    extensions: {
-        '.js': null
-    }
+  name: 'fisa',
+  processTitle: 'fisa',
+  moduleName: 'fisa',
+  configName: 'fis-conf',
+
+  // only js supported!
+  extensions: {
+    '.js': null
+  }
 });
 
 cli.launch({
-    cwd: argv.r || argv.root,
-    configPath: argv.f || argv.file
+  cwd: argv.r || argv.root,
+  configPath: argv.f || argv.file
 }, function(env) {
-    var fis = require(!env.modulePath ? '../index.js' : env.modulePath);
-    
-    process.title = this.name + ' ' + process.argv.slice(2).join(' ') + ' [ ' + env.cwd + ' ]';
-    /**
-     * 配置插件查找路径，优先查找本地项目里面的 node_modules，然后
-     * 才是全局环境下面安装的 fisa 目录里面的 node_modules
-    */
-    fis.require.paths.unshift(path.join(env.cwd, 'node_modules'));
-    fis.require.paths.push(path.join(path.dirname(__dirname), 'node_modules'));
-    fis.cli.name = this.name;
-    fis.cli.run(argv, env);
+  var fis;
+  if (!env.modulePath) {
+    fis = require('../');
+  } else {
+    fis = require(env.modulePath);
+  }
+
+  process.title = this.name +' ' + process.argv.slice(2).join(' ') + ' [ ' + env.cwd + ' ]';
+
+  // 配置插件查找路径，优先查找本地项目里面的 node_modules
+  // 然后才是全局环境下面安装的 fis3 目录里面的 node_modules
+  fis.require.paths.unshift(path.join(env.cwd, 'node_modules'));
+  fis.require.paths.push(path.join(path.dirname(__dirname), 'node_modules'));
+  fis.cli.name = this.name;
+  fis.cli.run(argv, env);
 });
